cc6c52b1ee92757c6d47bb014e37f0ce530e0aa2
cmihiranga099 <santhushweerakoon099@gmail.com>
2025-02-19 00:20:15 +0530

    Update Implementing_a_Deque_Using_Two_FIFO_Queues.java

--- Implementing_a_Deque_Using_Two_FIFO_Queues.java
+++ Implementing_a_Deque_Using_Two_FIFO_Queues.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package implementing_stacks_using_queue;
 import java.util.LinkedList;
 import java.util.Queue;
